00cd48e011d20ece7ee53d152db84b76af27128e
Andres Fernando <andresforonda.af@gmail.com>
2020-10-26 20:34:58 -0300

    Add on key press event listener and get its value
    Get the value from a key pressed as an event

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
+// Import config module
 const config = require("./config/index.js");
 
+/**
+ * Function to start the server application
+ */
 const initServer = async () => {
   try {
+    // Get the ari client instance
     const ari = await config.ari.connect;
-    ari.on("StasisStart", async () => {});
+    // Register the asterisk StasisStart event to detect a new incoming call
+    ari.on("StasisStart", async (event, incoming) => {
+      // Register the on press key event
+      incoming.on("ChannelDtmfReceived", (e, channel) => {
+        // Get the pressed key value from the channel event
+        const digit = e.digit;
+        console.log(digit);
+      });
+    });
+
+    // Start the tic-tac-toe webSocket in asterisk
     ari.start("tic-tac-toe");
   } catch (error) {
     console.log(error);
